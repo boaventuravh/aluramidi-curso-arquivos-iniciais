cdcf105c5b40e22c89aeef293812e259153462a5
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-03-18 10:17:07 -0300

    adicionar função de reprodução de som dinâmica

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function reproduzirSomClap(){
-    document.querySelector('#som_tecla_clap').play();
+//recebe o nome da classe do button e converte para o id das tags audio
+function gerarIdSomTecla(classTecla){
+    idTecla = classTecla.replace("tecla ", "som_");
+    return idTecla;
 }
 
+//identifica uma tag audio pelo id e reproduz o som
+function reproduzirSom(){
+    classTecla = gerarIdSomTecla(this.className);
+    idTecla = '#' + gerarIdSomTecla(classTecla);
+    document.querySelector(idTecla).play();
+}
+
+//armazena todas as teclas em uma lista
 const listaTeclas = document.querySelectorAll('.tecla');
+
+//atribui a função reproduzirSom ao onclick de todos os botões
+listaTeclas.forEach(tecla => {
+    tecla.onclick = reproduzirSom;
+});
