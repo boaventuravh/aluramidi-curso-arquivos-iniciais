ffff7fed3ff005e3e1013ce9f1ab12e8a94fe7ea
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-03-19 09:01:33 -0300

    reorganizar cod + retirar redundância

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+//identifica uma tag audio pelo id e reproduz o som
+function reproduzirSom(){
+    classTecla = gerarIdSomTecla(this.className);
+    idTecla = '#' + classTecla;
+    document.querySelector(idTecla).play();
+}
+
 //recebe o nome da classe do button e converte para o id das tags audio
 function gerarIdSomTecla(classTecla){
     idTecla = classTecla.replace("tecla ", "som_");
     return idTecla;
-}
-
-//identifica uma tag audio pelo id e reproduz o som
-function reproduzirSom(){
-    classTecla = gerarIdSomTecla(this.className);
-    idTecla = '#' + gerarIdSomTecla(classTecla);
-    document.querySelector(idTecla).play();
 }
 
 //armazena todas as teclas em uma lista
